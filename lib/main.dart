ff8b4eb245fd6ba80f4ebaae0edfcce0d3e8cef1
Basit Ali <basitali71019@gmail.com>
2023-09-26 21:11:08 +0500

    Gmail and SMS feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:call_button/gmail_and_sms_button.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'call_button.dart';
+
 
 
 void main() {
@@ -19,59 +21,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const GmailAndSmsButton(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  void _launchPhoneDialer() async {
-    const phoneNumber = '+923151234567';
-    final Uri url = Uri.parse('tel:$phoneNumber');
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    }
-    else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Error: Could not launch phone dialer.'),
-        ),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Call button',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _launchPhoneDialer,
-        tooltip: 'Dial',
-        child: const Icon(Icons.phone),
-      ),
-    );
-  }
-}
